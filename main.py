fc015cea12655cfecd55f89472ba9749b9f33b42
Lawliet94 <alexander.klaar.ak@gmail.com>
2025-11-08 17:42:49 +0100

    added sorting function to stats.py and able to make a first report

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sort_characters
 
 def get_book_text(book_path):
     with open(book_path, "r") as book:
         return book.read()
+    
+def report_results(text_path):
+    with open(text_path, "r") as text:
+        return text.read()
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    print(f"Found {count_words(book_text)} total words")
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    word_count = count_words(book_text)
     character_count = count_characters(book_text)
-    print(character_count)
+    sorted_characters = sort_characters(character_count)
+
+    report_path = "report.txt"
+    report = report_results(report_path)
+    print(report.format(book_path=book_path, word_count=word_count, sorted_characters=sorted_characters))
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,12 +1,33 @@
 def count_words(book_text):
+    """Return the number of words in the book."""
     return len(book_text.split())
 
 def count_characters(book_text):
+    """Return how many times each character appears in the book, treating uppercase and lowercase as the same."""
     score = {}
-
     for c in book_text:
         if c.lower() not in score.keys():
             score.update({c.lower(): 1})
         else:
             score[c.lower()] += 1
     return score
+
+def sort_characters(character_count):
+    """Return a string of alphabetic characters and their counts, sorted in descending order."""
+    sorted = []
+    sorted_dict = {}
+
+    for k, v in character_count.items():
+        if k.isalpha():
+            sorted.append({"char": k, "num": v})
+        else:
+            continue
+
+    def sort_on(items):
+        return items["num"]
+    
+    sorted.sort(reverse=True, key= sort_on)
+    for d in sorted:
+        sorted_dict.update({d["char"]: d["num"]})
+    
+    return "".join(f"{k}: {v}\n" for k, v in sorted_dict.items()) 
