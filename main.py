6642ad4bc865fb076759a734dde310cbf9c4929a
Lawliet94 <alexander.klaar.ak@gmail.com>
2025-11-09 14:21:21 +0100

    added sys.argv so users can specify which book to analyze

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words, count_characters, sort_characters
 
 def get_book_text(book_path):
@@ -9,7 +10,10 @@ def report_results(text_path):
         return text.read()
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     word_count = count_words(book_text)
     character_count = count_characters(book_text)
